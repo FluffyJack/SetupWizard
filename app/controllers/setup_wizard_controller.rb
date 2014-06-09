270c53841e5b672524bcd87788fff471a89229e4
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-06-09 16:08:03 +1000

    Changes from episode 28

--- app/controllers/setup_wizard_controller.rb
+++ app/controllers/setup_wizard_controller.rb
@@ -1,7 +1,43 @@
 class SetupWizardController < UIViewController
+  def init
+    super
+    @current_controller = -1
+    self
+  end
+
   def loadView
     super
 
     self.view = SetupWizardView.new
+
+    @nav = UINavigationController.alloc.initWithRootViewController(next_controller)
+    addChildViewController @nav
+
+    @nav.view.frame = view.content.bounds
+    view.content.addSubview @nav.view
+  end
+
+  def viewDidAppear(animated)
+    super
+
+    view.button.addTarget(self, action: 'next_step:', forControlEvents: UIControlEventTouchUpInside)
+  end
+
+  def next_step(sender)
+    @nav.pushViewController(next_controller, animated: true)
+  end
+
+  private
+
+  def next_controller
+    controllers[@current_controller += 1].new
+  end
+
+  def controllers
+    [
+      FirstStepController,
+      SecondStepController,
+      ThirdStepController
+    ]
   end
 end
